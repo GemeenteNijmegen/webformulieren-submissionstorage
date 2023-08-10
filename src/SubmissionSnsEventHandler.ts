cb16dad0a605210e4bc66918f73045373f0ed3fc
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2023-08-10 16:07:32 +0200

    fix: Allow lambda to read secret

---
+++ src/SubmissionSnsEventHandler.ts
@@ -0,0 +1,62 @@
+import { ITable, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Key } from 'aws-cdk-lib/aws-kms';
+import { RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { Bucket, IBucket } from 'aws-cdk-lib/aws-s3';
+import { ISecret, Secret } from 'aws-cdk-lib/aws-secretsmanager';
+import { ITopic, Topic } from 'aws-cdk-lib/aws-sns';
+import { LambdaSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
+import { StringParameter } from 'aws-cdk-lib/aws-ssm';
+import { Construct } from 'constructs';
+import { SubmissionFunction } from './app/submission/submission-function';
+import { Statics } from './statics';
+
+interface SubmissionSnsEventHandlerProps {
+  topicArn: string;
+}
+export class SubmissionSnsEventHandler extends Construct {
+  constructor(scope: Construct, id: string, props: SubmissionSnsEventHandlerProps) {
+    super(scope, id);
+
+    const topic = Topic.fromTopicArn(this, 'submission-topic', props.topicArn);
+    const table = Table.fromTableName(this, 'table', StringParameter.valueForStringParameter(this, Statics.ssmSubmissionTableName));
+    const key = Key.fromKeyArn(this, 'key', StringParameter.valueForStringParameter(this, Statics.ssmDataKeyArn));
+    // IBucket requires encryption key, otherwise grant methods won't add the correct permissions
+    const bucket = Bucket.fromBucketAttributes(this, 'bucket', {
+      bucketArn: StringParameter.valueForStringParameter(this, Statics.ssmSubmissionBucketArn),
+      encryptionKey: key,
+    });
+    const secret = Secret.fromSecretNameV2(this, 'apikey', Statics.secretFormIoApiKey);
+
+    this.submissionHandlerLambda(bucket, table, topic, secret);
+  }
+
+  /**
+   * This lambda handles submissions received via SNS
+   *
+   * It will receive each submission, extract relevant info, store
+   * the unedited submission in S3, retrieve attachments and store
+   * them, and add relevant metadata to a dynamoDB table for
+   * further use.
+   *
+   * @param bucket The bucket to store submission info / attachments in
+   * @param table The dynamodb table to store submission (meta)data in
+   * @param topic The SNS Topic to subscribe to for submissions
+   */
+  private submissionHandlerLambda(bucket: IBucket, table: ITable, topic: ITopic, secret: ISecret) {
+    const submissionLambda = new SubmissionFunction(this, 'submission', {
+      logRetention: RetentionDays.SIX_MONTHS,
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+        TABLE_NAME: table.tableName,
+        FORMIO_API_KEY_ARN: secret.secretArn,
+        FORMIO_BASE_URL: StringParameter.valueForStringParameter(this, Statics.ssmFormIoBaseUrl),
+      },
+    });
+    bucket.grantWrite(submissionLambda);
+    table.grantReadWriteData(submissionLambda);
+    secret.grantRead(submissionLambda);
+
+    topic.addSubscription(new LambdaSubscription(submissionLambda));
+  }
+
+}
