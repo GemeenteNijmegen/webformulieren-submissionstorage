faf5e70014dfc0b46a30253ae4daa516745a90fc
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2024-01-24 17:21:15 +0100

    use requesthandler

--- src/app/listSubmissions/listSubmissionsRequestHandler.ts
+++ src/app/listSubmissions/listSubmissionsRequestHandler.ts
@@ -1,6 +1,12 @@
+import { Response } from '@gemeentenijmegen/apigateway-http/lib/V2/Response';
+import { EventParameters } from './parsedEvent';
+
 export class ListSubmissionsRequestHandler {
+  constructor() {
+  }
 
-  constructor(userId: string) {
-
+  handleRequest(parameters: EventParameters): Response {
+    console.debug(parameters);
+    return Response.ok();
   }
 }
---
+++ src/app/listSubmissions/parsedEvent.ts
@@ -0,0 +1,15 @@
+import { APIGatewayProxyEventV2 } from 'aws-lambda';
+import { z } from 'zod';
+
+const EventParametersSchema = z.object({
+  userId: z.string(),
+  userType: z.enum(['organisation', 'person']),
+});
+export type EventParameters = z.infer<typeof EventParametersSchema>;
+
+export function parsedEvent(event: APIGatewayProxyEventV2): EventParameters {
+  return EventParametersSchema.passthrough().parse({
+    userId: event.queryStringParameters?.user_id,
+    userType: event.queryStringParameters?.user_type,
+  });
+}
