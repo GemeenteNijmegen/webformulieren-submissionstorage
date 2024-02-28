fc757e9a54326baa043a370407bd181a301e0961
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2024-02-28 16:00:32 +0100

    feat: Log event in access control lambda

--- src/app/storageAccessControl/storageAccessControl.lambda.ts
+++ src/app/storageAccessControl/storageAccessControl.lambda.ts
@@ -1,7 +1,7 @@
 import { CloudFrontRequestEvent, CloudFrontResponse } from 'aws-lambda';
 
 export async function handler(_event: CloudFrontRequestEvent): Promise<CloudFrontResponse> {
-
+  console.debug(_event);
   return {
     status: '403',
     statusDescription: 'No access allowed',
