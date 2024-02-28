5ad7dca7bb917565c94503baae17a3012735d214
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2024-02-28 16:36:29 +0100

    feat: test with 403

--- src/app/storageAccessControl/storageAccessControl.lambda.ts
+++ src/app/storageAccessControl/storageAccessControl.lambda.ts
@@ -3,7 +3,7 @@ import { CloudFrontRequestEvent, CloudFrontResponse } from 'aws-lambda';
 export async function handler(_event: CloudFrontRequestEvent): Promise<CloudFrontResponse> {
   console.debug(JSON.stringify(_event));
   return {
-    status: '200',
+    status: '403',
     statusDescription: 'OK',
     headers: {},
   };
