65b4241927209300f8391dee73fab30dbb661aa7
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2024-02-28 16:15:27 +0100

    feat: Test with 200

--- src/app/storageAccessControl/storageAccessControl.lambda.ts
+++ src/app/storageAccessControl/storageAccessControl.lambda.ts
@@ -1,10 +1,10 @@
 import { CloudFrontRequestEvent, CloudFrontResponse } from 'aws-lambda';
 
 export async function handler(_event: CloudFrontRequestEvent): Promise<CloudFrontResponse> {
-  console.debug(_event);
+  console.debug(JSON.stringify(_event));
   return {
-    status: '403',
-    statusDescription: 'No access allowed',
+    status: '200',
+    statusDescription: 'OK',
     headers: {},
   };
 }
