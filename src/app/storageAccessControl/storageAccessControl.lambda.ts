beabab0a49fafc976dee95c4edac365887ae442e
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2024-02-28 16:52:41 +0100

    fix: test with hardcoded uri

--- src/app/storageAccessControl/storageAccessControl.lambda.ts
+++ src/app/storageAccessControl/storageAccessControl.lambda.ts
@@ -1,7 +1,12 @@
 import { CloudFrontRequestEvent, CloudFrontResponse } from 'aws-lambda';
 
-export async function handler(_event: CloudFrontRequestEvent): Promise<CloudFrontResponse> {
-  console.debug(JSON.stringify(_event));
+export async function handler(event: CloudFrontRequestEvent, _context: any, callback: any): Promise<CloudFrontResponse> {
+  console.debug(JSON.stringify(event));
+  const request = event.Records[0].cf.request;
+  if (request.uri.startsWith('/APV18.605')) {
+    callback(null, request);
+  }
+
   return {
     status: '403',
     statusDescription: 'OK',
