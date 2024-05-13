afd12c52baebd5a0360bd5b05da24bf6b3d9bcb4
Wieteke <149399985+WietekeNijmegen@users.noreply.github.com>
2024-05-13 16:06:28 +0200

    feat: added unit test fix again to enable build to revert secondary global index in main

--- src/app/submission/test/submissionHandler.test.ts
+++ src/app/submission/test/submissionHandler.test.ts
@@ -4,6 +4,18 @@ import { GetSecretValueCommand, SecretsManagerClient } from '@aws-sdk/client-sec
 import { mockClient } from 'aws-sdk-client-mock';
 import * as snsSample from './samples/sns.sample.json';
 import { SubmissionHandler } from '../SubmissionHandler';
+
+let mockDefinition = jest.fn().mockResolvedValue({ title: 'testTitel', name: 'testName' });
+jest.mock('../FormConnector', () => {
+  return {
+    FormIoFormConnector: jest.fn(() => {
+      return {
+        definition: mockDefinition,
+      };
+    }),
+  };
+});
+
 const secretsMock = mockClient(SecretsManagerClient);
 const dbMock = mockClient(DynamoDBClient);
 const s3Mock = mockClient(S3Client);
