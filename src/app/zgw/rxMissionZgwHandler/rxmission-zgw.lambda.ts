ff507128e0fd78b0c579fbd7064a2f5e636f0844
Wieteke <w.kremer@nijmegen.nl>
2024-11-21 13:21:18 +0100

    feat: mocks based on json of submissions

--- src/app/zgw/rxMissionZgwHandler/rxmission-zgw.lambda.ts
+++ src/app/zgw/rxMissionZgwHandler/rxmission-zgw.lambda.ts
@@ -14,10 +14,19 @@ if (process.env.DEBUG !== 'true') {
 const env = environmentVariables(['BRANCH']);
 const rxMissionZgwConfig = getRxMissionZgwConfiguration(env.BRANCH!);
 
-export async function handler(event: EventBridgeEvent<'New Form Processed', {Reference: string; UserId: string; UserType: 'person' | 'organisation'; Key: string}>) {
+export async function handler(event: RxMissionProcessedFormEvent) {
   console.log('RxMission Event Detected and ready for processing', event);
   // parse event with zod
   const rxMissionZgwHandler = new RxMissionZgwHandler(rxMissionZgwConfig);
   await rxMissionZgwHandler.sendSubmissionToRxMission(event.detail.Key, event.detail.UserId, event.detail.UserType);
 
 }
+
+export interface RxMissionEventDetail {
+  Reference: string;
+  UserId: string;
+  UserType: 'person' | 'organisation';
+  Key: string;
+}
+
+export type RxMissionProcessedFormEvent = EventBridgeEvent<'New Form Processed', RxMissionEventDetail>;
