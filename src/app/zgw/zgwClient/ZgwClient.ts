4ba6d24eac320d817c9067aea2543b5e7b797f0c
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2024-12-10 11:17:32 +0100

    debug: Add roletype from config

--- src/app/zgw/zgwClient/ZgwClient.ts
+++ src/app/zgw/zgwClient/ZgwClient.ts
@@ -252,7 +252,7 @@ export class ZgwClient {
       roltoelichting: 'aanvrager',
       betrokkeneIdentificatie,
     };
-
+    
     // Construct the contactpersoonRol object.
     // When no name is provided keep it undefined as this is a required field.
     let contactpersoonRol: any = {
@@ -261,9 +261,10 @@ export class ZgwClient {
       naam: name,
     };
     if (!name) {
+      console.debug('Not adding contactpersoon because of missing name');
       contactpersoonRol = undefined;
     }
-
+    console.debug(roleRequest);
     return this.callZaakApi(HttpMethod.Post, 'rollen', {
       ...roleRequest,
       contactpersoonRol,
