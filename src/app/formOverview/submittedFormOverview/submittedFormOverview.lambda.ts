d80fe255c4efd7154763639bd854d96f9be8d714
WietekeNijmegen <wietekenijmegen@gmail.com>
2025-06-24 07:48:57 +0200

    feat: optional params for submittedFormOverview to easily count multiple forms over a custom period

--- src/app/formOverview/submittedFormOverview/submittedFormOverview.lambda.ts
+++ src/app/formOverview/submittedFormOverview/submittedFormOverview.lambda.ts
@@ -1,64 +1,106 @@
-import { SSM } from '@aws-sdk/client-ssm';
-import { environmentVariables, S3Storage, Storage } from '@gemeentenijmegen/utils';
-import { Statics } from '../../../statics';
-import { Database, DynamoDBDatabase } from '../../submission/Database';
-import { FormOverviewDatabase, DDBFormOverviewDatabase } from '../database/FormOverviewDatabase';
+import { SSM } from "@aws-sdk/client-ssm";
+import {
+  environmentVariables,
+  S3Storage,
+  Storage,
+} from "@gemeentenijmegen/utils";
+import { Statics } from "../../../statics";
+import { Database, DynamoDBDatabase } from "../../submission/Database";
+import {
+  FormOverviewDatabase,
+  DDBFormOverviewDatabase,
+} from "../database/FormOverviewDatabase";
 
 const envKeys = [
-  'TABLE_NAME',
-  'DOWNLOAD_BUCKET_NAME',
-  'FORM_OVERVIEW_TABLE_NAME',
+  "TABLE_NAME",
+  "DOWNLOAD_BUCKET_NAME",
+  "FORM_OVERVIEW_TABLE_NAME",
 ] as const;
 
 /**
  * Retrieve an overview of all submitted forms with basic information.
  * Overviews can be used to check if all submitted forms have been processed properly in the backend.
  * Only based on database information (not S3 Bucket) and saved to the downloads.
+ *  * Optional event parameters:
+ * - startDate (string in YYYY-MM-DD format) The newest date. Starts today for example.
+ * - endDate (string in YYYY-MM-DD format) The one in the past. Today - 40 days for example.
+ * - formNames (comma-separated string of form names)
  */
-export async function handler(_event: any) {
+export async function handler(event: any) {
   const env = environmentVariables(envKeys);
   const database = new DynamoDBDatabase(env.TABLE_NAME);
   const downloadStorage = new S3Storage(env.DOWNLOAD_BUCKET_NAME);
-  const formOverviewDatabase = new DDBFormOverviewDatabase(env.FORM_OVERVIEW_TABLE_NAME);
+  const formOverviewDatabase = new DDBFormOverviewDatabase(
+    env.FORM_OVERVIEW_TABLE_NAME
+  );
 
   try {
-    const { startDate, endDate } = getLast30DaysDateRange();
+    let startDate: string;
+    let endDate: string;
 
-    // Alle subsidieformulieren
-    // Retrieve form names directly from SSM, needs no caching. Lambda hardly ever runs.
-    const ssm = new SSM();
-    const formNamesParam = await ssm.getParameter({ Name: Statics.ssmSubmittedFormoverviewFormnames });
-    const formNames = formNamesParam?.Parameter?.Value?.split(',').map(name => name.trim()) || [];
+    if (
+      event?.startDate &&
+      event?.endDate &&
+      isValidDate(event.startDate) &&
+      isValidDate(event.endDate)
+    ) {
+      startDate = event.startDate;
+      endDate = event.endDate;
+      console.log(
+        `Getting submitted form overview for custom range. ${endDate} to ${startDate}`
+      );
+    } else {
+      const dateRange = getLast30DaysDateRange();
+      startDate = dateRange.startDate;
+      endDate = dateRange.endDate;
+    }
 
+    let formNames: string[];
+    if (event?.formNames && typeof event?.formNames === "string") {
+      formNames = event.formNames.split(",").map((name: string) => name.trim());
+    } else {
+      // Alle subsidieformulieren
+      // Retrieve form names directly from SSM, needs no caching. Lambda hardly ever runs.
+      const ssm = new SSM();
+      const formNamesParam = await ssm.getParameter({
+        Name: Statics.ssmSubmittedFormoverviewFormnames,
+      });
+      formNames =
+        formNamesParam?.Parameter?.Value?.split(",").map((name) =>
+          name.trim()
+        ) || [];
+    }
 
     if (!formNames || formNames.length === 0) {
-      throw new Error('No form names available for query.');
+      throw new Error("No form names available for query.");
     }
 
     // Get all submitted forms for the past 30 days
     const submissions = (
       await Promise.all(
-        formNames.map(formName =>
+        formNames.map((formName) =>
           getFormSubmissionsFromDatabase(database, {
             formName,
             startDate,
             endDate,
-          }),
-        ),
+          })
+        )
       )
     ).flat();
 
     if (submissions.length === 0) {
       return {
         statusCode: 204,
-        body: JSON.stringify({ message: 'No submissions found in the past 30 days.' }),
+        body: JSON.stringify({
+          message: "No submissions found in the past 30 days.",
+        }),
       };
     }
 
     // Compile CSV from submissions
     const csvContent = await compileCsvFile([
-      ['Key', 'Date Submitted', 'Form Name', 'Form Title'], // Headers
-      ...submissions.map(submission => [
+      ["Key", "Date Submitted", "Form Name", "Form Title"], // Headers
+      ...submissions.map((submission) => [
         submission.key,
         submission.dateSubmitted,
         submission.formName,
@@ -67,20 +109,31 @@ export async function handler(_event: any) {
     ]);
 
     // Save the CSV file to S3 and database
-    const csvFileName = await saveCsvFile(downloadStorage, formOverviewDatabase, csvContent, {
-      startDate,
-      endDate,
-    });
+    const csvFileName = await saveCsvFile(
+      downloadStorage,
+      formOverviewDatabase,
+      csvContent,
+      {
+        startDate,
+        endDate,
+      }
+    );
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'CSV generated successfully.', fileName: csvFileName }),
+      body: JSON.stringify({
+        message: "CSV generated successfully.",
+        fileName: csvFileName,
+      }),
     };
   } catch (error) {
-    console.error('Error generating form overview:', error);
+    console.error("Error generating form overview:", error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: 'Problemen bij het maken van het submitted formoverview overzicht' }),
+      body: JSON.stringify({
+        message:
+          "Problemen bij het maken van het submitted formoverview overzicht",
+      }),
     };
   }
 }
@@ -89,9 +142,9 @@ export async function handler(_event: any) {
  * Compiles an array of strings into a CSV format.
  */
 async function compileCsvFile(submissionsArray: string[][]): Promise<string> {
-  let csvContent = '';
-  submissionsArray.forEach(row => {
-    csvContent += row.join(';') + '\n';
+  let csvContent = "";
+  submissionsArray.forEach((row) => {
+    csvContent += row.join(";") + "\n";
   });
   return csvContent;
 }
@@ -103,7 +156,7 @@ async function saveCsvFile(
   downloadStorage: Storage,
   formOverviewDatabase: FormOverviewDatabase,
   csvFile: string,
-  params: { startDate: string; endDate: string },
+  params: { startDate: string; endDate: string }
 ): Promise<string> {
   const epochTime = new Date().getTime();
   const csvFileName = `SubmittedFormOverview-${epochTime}.csv`;
@@ -111,9 +164,9 @@ async function saveCsvFile(
   await downloadStorage.store(csvFileName, csvFile);
   await formOverviewDatabase.storeFormOverview({
     fileName: csvFileName,
-    createdBy: 'event', // Replace this with dynamic user information if needed
-    formName: 'overview',
-    formTitle: 'Submitted Form Overview',
+    createdBy: "event", // Replace this with dynamic user information if needed
+    formName: "overview",
+    formTitle: "Submitted Form Overview",
     queryStartDate: params.startDate,
     queryEndDate: params.endDate,
   });
@@ -126,14 +179,19 @@ async function saveCsvFile(
  * @param formName
  * @returns
  */
-async function getFormSubmissionsFromDatabase(database: Database, params: GetSubmissionsByFormNameParams) {
+async function getFormSubmissionsFromDatabase(
+  database: Database,
+  params: GetSubmissionsByFormNameParams
+) {
   const databaseResult = await database.getSubmissionsByFormName({
     formName: params.formName,
     startDate: params.startDate,
     endDate: params.endDate,
   });
   if (!databaseResult || !Array.isArray(databaseResult)) {
-    throw Error(`Cannot retrieve submitted forms for ${params.formName}. DatabaseResult is false or not the expected array. No results would give an empty array, not this error.`);
+    throw Error(
+      `Cannot retrieve submitted forms for ${params.formName}. DatabaseResult is false or not the expected array. No results would give an empty array, not this error.`
+    );
   }
   // No results from database should return an empty object to throw a 204
   if (!databaseResult.length) return [];
@@ -152,7 +210,7 @@ async function getFormSubmissionsFromDatabase(database: Database, params: GetSub
  */
 function getLast30DaysDateRange(): { startDate: string; endDate: string } {
   const today = new Date();
-  const startDate = today.toISOString().split('T')[0]; // Format: YYYY-MM-DD
+  const startDate = today.toISOString().split("T")[0]; // Format: YYYY-MM-DD
 
   // Subtract 30 days from today
   const endDate = new Date(today);
@@ -160,9 +218,13 @@ function getLast30DaysDateRange(): { startDate: string; endDate: string } {
 
   // Return both dates in YYYY-MM-DD format
   return {
-    endDate: endDate.toISOString().split('T')[0],
+    endDate: endDate.toISOString().split("T")[0],
     startDate,
   };
+}
+
+function isValidDate(value: string): boolean {
+  return /^\d{4}-\d{2}-\d{2}$/.test(value) && !isNaN(Date.parse(value));
 }
 
 export interface GetSubmissionsByFormNameParams {
